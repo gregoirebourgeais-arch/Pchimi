0ceffe753a9eebcc2b731688f907f99a55d24d8e
TiMamGoz <gregoire.bourgeais@gmail.com>
2026-03-18 03:30:52 +0100

    Build family menu planner with recipes and grocery list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,8 @@
 import "@/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TrainingBooklet from "@/pages/TrainingBooklet";
+import MenuPlanner from "@/pages/MenuPlanner";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-background">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<TrainingBooklet />} />
-          <Route path="/page/:pageId" element={<TrainingBooklet />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  return <MenuPlanner />;
 }
 
 export default App;
